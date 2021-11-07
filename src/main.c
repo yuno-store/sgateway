7da20d485d6ab19a4ab944f23beadd14900ee3b8
Niyamaka <niyamaka@yuneta.io>
2021-11-07 11:17:08 +0100

    v5.0.1

--- src/main.c
+++ src/main.c
@@ -30,7 +30,7 @@
 #define APP_NAME        ROLE_SGATEWAY
 #define APP_DOC         "Simple Gateway"
 
-#define APP_VERSION     "5.0.0"
+#define APP_VERSION     "5.0.1"
 #define APP_SUPPORT     "<niyamaka at yuneta.io>"
 #define APP_DATETIME    __DATE__ " " __TIME__
 
